ed4c8b5eb6bab872da97e8d94681f9c8c03ba25b
rgsax1 <rgsa@outlook.com.br>
2023-11-11 16:40:42 -0300

    starting project

--- employee-spring/src/main/java/rodrigo/salles/employeespring/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package rodrigo.salles.employeespring.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
